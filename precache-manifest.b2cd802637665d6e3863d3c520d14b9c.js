e5b7528750423c1421b36a70f8d198ceb6a0bda4
Alec Waters <alech2os@uw.edu>
2019-05-02 11:11:24 -0700

    Updates

---
+++ precache-manifest.b2cd802637665d6e3863d3c520d14b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa33ab5f79ba4b0d98df955e8430cca",
+    "url": "/fricreact-gh-pages/index.html"
+  },
+  {
+    "revision": "54754f30201790bfbbb9",
+    "url": "/fricreact-gh-pages/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/fricreact-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "54754f30201790bfbbb9",
+    "url": "/fricreact-gh-pages/static/js/main.77ada4e4.chunk.js"
+  },
+  {
+    "revision": "4de0379f302723d693b7",
+    "url": "/fricreact-gh-pages/static/js/runtime~main.f9b02d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fricreact-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
